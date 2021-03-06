c5d3aa8573ee9b26c1ffbd8baa9b507fab97a4b2
mfhillows <65815172+mfhillows@users.noreply.github.com>
2021-03-06 21:43:34 +0200

    start add
    Закончил третий раздел. Разобрался.

---
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
